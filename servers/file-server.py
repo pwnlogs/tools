370144442ca1b6e304d6b6ba2abca324d191a52f
Jithin Pavithran <13626831+JithinPavithran@users.noreply.github.com>
2022-03-21 16:04:03 +0530

    added logging to file-server-ssl

--- servers/file-server.py
+++ servers/file-server.py
@@ -3,7 +3,7 @@ A Flask file server with CORS support.
 """
 import sys
 import flask
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__,
@@ -13,13 +13,25 @@ app = Flask(__name__,
 
 app.secret_key = "this is only for testing"
 CORS(app, send_wildcard=True, expose_headers=[])
+count = 0
 
+@app.after_request
+def process(response):
+    global count
+    print('\n============== Request Count:' + str(count) + ' ==============')
+    count = count + 1
+    # log the request
+    print(request.method + ' ' + request.full_path)
+    for h in request.headers:
+        print(h[0] + ': ' + h[1])
+    print('\n' + request.stream.read().decode(), file=sys.stderr)
+    # return original response
+    return response
 
 @app.after_request
 def apply_caching(response):
     response.headers["Custom-Header"] = "Custom-Value"
     return response
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='80')
