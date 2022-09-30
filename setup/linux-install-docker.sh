fb623cf2e91dfb4021e5c4e27c7377a99b56c27e
Jithin Pavithran <13626831+pwnlogs@users.noreply.github.com>
2022-09-30 23:04:07 +0200

    Update linux-install-docker.sh

--- setup/linux-install-docker.sh
+++ setup/linux-install-docker.sh
@@ -11,4 +11,4 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
+sudo usermod -a -G  docker $USER
