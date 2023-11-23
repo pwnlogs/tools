b71655c173af90612b6f83e42aaf203a32412c0a
Jithin Pavithran <13626831+pwnlogs@users.noreply.github.com>
2023-11-23 22:08:27 +0100

    Update create-certificate.sh

--- ca/create-certificate.sh
+++ ca/create-certificate.sh
@@ -3,8 +3,8 @@
 # create certificate for a domain
 
 # verify arguments
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 domain-name"
+if [ "$#" -le 0 ]; then
+    echo "Usage: $0 domain-name-1 [alt-domain-name-2] [alt-domain-name-3] [alt-domain-name-4]"
     echo "    domain-name: the domain for which the certificate should be issued."
     exit
 fi
@@ -29,10 +29,25 @@ subjectAltName = @alt_names
 DNS.1 = ${1}
 EOL
 
+
+if [ "$#" -ge 1 ]; then
+    echo "DNS.2 = ${2}" >> "${1}.ext"
+    if [ "$#" -ge 2 ]; then
+        echo "DNS.3 = ${3}" >> "${1}.ext"
+        if [ "$#" -ge 3 ]; then
+            echo "DNS.4 = ${4}" >> "${1}.ext" 
+            if [ "$#" -ge 4 ]; then
+                echo "Only 4 subject names are supported"
+                exit
+            fi
+        fi
+    fi
+fi
+
 # create signed certificate
 openssl x509 -req -in "${1}.csr" \
     -CA root-ca-certificate.crt -CAkey root-ca-private-key.pem \
-    -CAcreateserial    -out "${1}.crt"    -days 825    -sha256 \
+    -CAcreateserial    -out "${1}.crt"    -days 360    -sha256 \
     -extfile "${1}.ext"
 
 # set permissions
