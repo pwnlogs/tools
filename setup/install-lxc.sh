fabab01cd3a7ccb2eaf38cc6e77381908dc4e539
Jithin Pavithran <13626831+pwnlogs@users.noreply.github.com>
2024-05-16 14:11:57 +0200

    Update install-lxc.sh

--- setup/install-lxc.sh
+++ setup/install-lxc.sh
@@ -16,3 +16,11 @@ lxc launch ubuntu:24.04 dev # dev is the container name
 lxc config set dev security.nesting true
 # Mark the container as privileged
 lxc config set dev security.privileged true
+
+# Allowing GUI apps on container
+# allow root user (lxc is run as root) to impersonate user's UID ($UID, usually 1000)
+echo "root:$UID:1" | sudo tee -a /etc/subuid /etc/subgid
+# create GUI profile
+lxc profile create gui-audio
+curl https://raw.githubusercontent.com/pwnlogs/tools/main/setup/lxc-gui-audio-profile.yaml | lxc profile edit gui-audio
+
