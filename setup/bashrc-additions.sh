b16f73df04d5f47d8150b4b8a53714e407ba6d4f
Jithin Pavithran <13626831+pwnlogs@users.noreply.github.com>
2024-05-16 13:05:43 +0200

    Update bashrc-additions.sh

--- setup/bashrc-additions.sh
+++ setup/bashrc-additions.sh
@@ -6,3 +6,11 @@ alias nmap='sudo nmap'
 alias cpr='cp -r -u'
 alias lxc-dev='lxc exec dev -- sudo --user ubuntu --login bash'
 # <<< aliases
+
+# >>> defining functions
+ports-from-nmap () { cat $1 | grep -E '/(tcp|udp)\s+open' | cut -d'/' -f1 | sort | uniq | tr '\n' ',' | head -c -1; }
+# <<< defining functions done
+
+# >>> define $PS1
+export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\$ '
+# <<< define $PS1
