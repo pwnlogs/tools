31cfffc48aa1c9ab35bd94031a62c76b0b0ead8f
Jithin Pavithran <13626831+pwnlogs@users.noreply.github.com>
2024-05-16 13:01:52 +0200

    Create bashrc-additions.sh

---
+++ setup/bashrc-additions.sh
@@ -0,0 +1,8 @@
+# >>> aliases
+alias l='ls -la'
+alias vi='vim'
+alias python='python3'
+alias nmap='sudo nmap'
+alias cpr='cp -r -u'
+alias lxc-dev='lxc exec dev -- sudo --user ubuntu --login bash'
+# <<< aliases
